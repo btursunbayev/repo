1e67961aaa0b04cb6d350cafadb4ee522f5b854b
Bekmukhamed <bekmukhamed.tursunbayev@gmail.com>
2025-11-25 20:18:42 -0800

    fix

--- add_rules_s1.sh
+++ add_rules_s1.sh
@@ -6,7 +6,7 @@ table_add ipv6_host forward 2001:db8:28fe:5613::3e2d:df8e => 1c:88:18:16:d2:41 1
 table_add ipv6_host forward 2001:db8:28fe:5613::fb6c:822 => 04:66:be:6a:81:dc 2
 table_add ipv6_host forward 2001:db8:28fe:5613::c28e:73e3 => d0:71:79:3d:7e:0a 3
 
-table_add ipv6_lpm forward 2001:db8:347c:d902::/64 => f0:b6:f2:1f:4e:1b 4
+table_add ipv6_lpm forward 2001:db8:347c:d902::/64 => 80:ce:01:83:8c:e4 4
 
 table_add port_filter allow_port 517 =>
 table_add port_filter allow_port 648 =>
--- add_rules_s2.sh
+++ add_rules_s2.sh
@@ -5,7 +5,7 @@ simple_switch_CLI << _EOF_
 table_add ipv6_host forward 2001:db8:347c:d902::840:5a3b => c4:9b:87:6c:82:15 1
 table_add ipv6_host forward 2001:db8:347c:d902::9610:d3ec => d4:bb:4e:80:f5:23 2
 
-table_add ipv6_lpm forward 2001:db8:28fe:5613::/64 => 7c:ee:f9:5d:ba:3a 4
+table_add ipv6_lpm forward 2001:db8:28fe:5613::/64 => 20:2f:38:b6:87:8a 4
 
 table_add port_filter allow_port 45 =>
 table_add port_filter allow_port 178 =>
